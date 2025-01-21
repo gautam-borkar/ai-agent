691a97640ca1203bbc7f64bbafacb7be7d4eafd3
Gautam Borkar <gborkar@us.ibm.com>
2025-01-21 16:38:26 -0500

    Added langraph support to chain.py

--- langgraph/chain.py
+++ langgraph/chain.py
@@ -1,19 +1,42 @@
 
 from langchain_ollama import ChatOllama
+from langchain_core.tools import tool
 from langchain_core.messages import AIMessage, HumanMessage
 
 from langgraph.graph import MessagesState, StateGraph, START, END
 
-def multiply(a:int, b:int) -> int:
-  """Multiple a and b
-  
-  Args:
-    a: First int
-    b: Second int
-  """
-  return a * b
+llm = ChatOllama(model="llama3.3")
 
-llm_with_tools = ChatOllama(model="llama3.2", temperature=0).bind_tools(tools=[multiply])
+@tool
+def multiply(a: int, b: int) -> int:
+    """Multiply a and b.
+
+    Args:
+        a: first int
+        b: second int
+    """
+    return a * b
+
+llm_with_tools = llm.bind_tools([multiply])
+
+class MessagesState(MessagesState):
+    # Add any keys needed beyond messages, which is pre-built 
+    pass
+
+# Node
+def tool_calling_llm(state: MessagesState):
+    return {"messages": [llm_with_tools.invoke(state["messages"])]}
+
+# Build graph
+builder = StateGraph(MessagesState)
+builder.add_node("tool_calling_llm", tool_calling_llm)
+builder.add_edge(START, "tool_calling_llm")
+builder.add_edge("tool_calling_llm", END)
+graph = builder.compile()
+
+messages = graph.invoke({"messages": HumanMessage(content="Hello!")})
+for m in messages['messages']:
+    m.pretty_print()
 
 class MessagesState(MessagesState):
     # Add any keys needed beyond messages, which is pre-built 
@@ -38,6 +61,6 @@ messages = graph.invoke({"messages": HumanMessage(content="Hello!")})
 for m in messages['messages']:
     m.pretty_print()
 
-messages = graph.invoke({"messages": HumanMessage(content="Multiply 2 and 3")})
+messages = graph.invoke({"messages": HumanMessage(content="Multiply 4 and 6")})
 for m in messages['messages']:
     m.pretty_print()
